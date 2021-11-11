f24b101a40952cf378d348f1a020a385c325056b
Fran <fmjerez@icloud.com>
2021-11-11 14:00:44 +0100

    Componentes, routing, html listo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Tareas from "./components/Tareas";
+import NuevaTarea from "./components/NuevaTarea";
+import EditarTarea from "./components/EditarTarea";
+import "./index.css";
 function App() {
-  return <h1> Crud - DynamizaTic </h1>;
+  return (
+    <Router>
+      <Header />
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Tareas} />
+          <Route exact path="/tarea/nueva" component={NuevaTarea} />
+          <Route exact path="/tarea/editar/:id" component={EditarTarea} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
